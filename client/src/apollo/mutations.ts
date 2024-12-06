ce3cb7ef77c97a7ce0393fd10195fe3beede6f14
sketchyTK <antkinney@gmail.com>
2024-12-05 19:52:28 -0600

    updating client files

---
+++ client/src/apollo/mutations.ts
@@ -0,0 +1,27 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email password: $password) {
+      token
+      User {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const ADDUSER = gql`
+  mutation addUser($input: UserInput!) {
+    addUser(input: $input) {
+      token
+        user{
+            _id
+            username
+            email
+        }
+    }
+  }
+`;
